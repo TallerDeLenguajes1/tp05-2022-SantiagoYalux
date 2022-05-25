3962bd8d34f17d28c6fa171baf161220f0083337
SantiagoYalux <santiyalux2@gmail.com>
2022-05-25 00:01:54 -0300

    Ejercicio 1

---
+++ Punto1/Punto1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num = 0;
+Console.WriteLine("ingresa el numero ");
+num = int.Parse(Console.ReadLine());
+
+
+int aux = num;
+int numInvert = 0;
+while (aux > 0)
+{
+    numInvert = numInvert * 10 + aux % 10;
+    aux = aux / 10;
+}
+
+Console.WriteLine("numero original: " + num.ToString());
+Console.WriteLine("numero modificado: " + numInvert.ToString());
