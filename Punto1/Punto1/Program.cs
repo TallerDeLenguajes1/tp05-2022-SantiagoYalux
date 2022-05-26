e38f741989cad7f77720d126d4af6c6f2680c303
SantiagoYalux <santiyalux2@gmail.com>
2022-05-26 19:03:29 -0300

    Calculadora actualizada, CalculadoraV2

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -92,9 +92,43 @@ do
 
             Console.ReadKey();
             break;
+        case ConsoleKey.E:
+            //Opción de Valor Abs, Cuadrado, Seno... 
+            Console.Clear();
+            Console.WriteLine("Ingresa el numero");
+            num1 = float.Parse(Console.ReadLine());
+            OperacionesVariadas(num1);
+
+            Console.ReadKey();
+            break;
+
+        case ConsoleKey.F:
+            //Opción de Máximo entre dos números 
+            Console.Clear();
+            Console.WriteLine("Ingresa el primer número");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo número");
+            num2 = float.Parse(Console.ReadLine());
+
+            MaximoDosNumeros(num1, num2);
+
+            Console.ReadKey();
+            break;
+        case ConsoleKey.G:
+            //Opción de Máximo entre dos números 
+            Console.Clear();
+            Console.WriteLine("Ingresa el primer número");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo número");
+            num2 = float.Parse(Console.ReadLine());
+
+            MinimoDosNumeros(num1, num2);
+
+            Console.ReadKey();
+            break;
 
     }
-} while (op.Key != ConsoleKey.E);
+} while (op.Key != ConsoleKey.H);
 
 
 float SumarDosNumeros(float num1, float num2)
@@ -117,6 +151,41 @@ float Dividir(float num1, float num2)
     return num1 / num2;
 }
 
+void OperacionesVariadas(float num1)
+{
+    Console.WriteLine($"El valor absoluto de {num1} = {Math.Abs(num1)}");
+    Console.WriteLine($"El cuadrado de {num1} = {Math.Pow(num1, 2)}");
+
+    if (num1 > 0)
+        Console.WriteLine($"La raiz cuadrada de {num1} = {Math.Sqrt(num1)}");
+    else
+        Console.WriteLine("No podemos conocer la raiz cuadrada");
+
+    Console.WriteLine($"El seno de {num1} = {Math.Sin(num1)}");
+    Console.WriteLine($"El Coseno de {num1} = {Math.Cos(num1)}");
+    Console.WriteLine($"La parte entera de {num1} es = {Math.Truncate(num1)}");
+}
+
+void MaximoDosNumeros(float num1, float num2)
+{
+    if (num1 > num2)
+        Console.WriteLine($"El mayor número entre {num1} y {num2} es = {num1}");
+    else if (num1 == num2)
+        Console.WriteLine($"{num1} y {num2} son iguales");
+    else
+        Console.WriteLine($"El mayor número entre {num1} y {num2} es = {num2}");
+}
+
+void MinimoDosNumeros(float num1, float num2)
+{
+    if (num1 > num2)
+        Console.WriteLine($"El menor número entre {num1} y {num2} es = {num2}");
+    else if (num1 == num2)
+        Console.WriteLine($"{num1} y {num2} son iguales");
+    else
+        Console.WriteLine($"El menor número entre {num1} y {num2} es = {num1}");
+
+}
 
 #region Menu
 class Menu
@@ -132,7 +201,10 @@ class Menu
         Console.WriteLine("B- Restar\n");
         Console.WriteLine("C- Multiplicar\n");
         Console.WriteLine("D- Dividir\n");
-        Console.WriteLine("E- Salir\n");
+        Console.WriteLine("E- Valor Abs, Cuadrado, Seno...\n");
+        Console.WriteLine("F- Máximo entre dos números\n");
+        Console.WriteLine("G- Mínimo entre dos números\n");
+        Console.WriteLine("H- Salir\n");
         Console.WriteLine("*************************");
 
     }
